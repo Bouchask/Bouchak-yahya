c9b15e2f622d92c82f64a0780d8647d29b1854b5
Bouchask <boyahya643@gmail.com>
2025-04-27 16:33:22 +0100

    update: modifications

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/Bouchak-yahya/', 
+  plugins: [react()],
+});
+
+  
